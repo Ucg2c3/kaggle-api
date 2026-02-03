1d046c42f32b9968174f700cdd98c0d145265eaa
stevemessick <stevemessick@users.noreply.github.com>
2026-02-03 10:21:35 -0800

    Restore model validation check (#902)
    Fixes #900

--- src/kaggle/api/kaggle_api_extended.py
+++ src/kaggle/api/kaggle_api_extended.py
@@ -3127,7 +3127,7 @@ class KaggleApi:
 
         model_sources = cast(List[str], self.get_or_default(meta_data, "model_sources", []))
         for source in model_sources:
-            self.validate_model_string(source)
+            self.validate_model_instance_version_string(source)
 
         docker_pinning_type = self.get_or_default(meta_data, "docker_image_pinning_type", None)
         if docker_pinning_type is not None and docker_pinning_type not in self.valid_push_pinning_types:
